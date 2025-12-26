59b36b219103b0f6c23b41cfdff0ea5f52010347
Levan Ilashvili <levanilashvili2005@gmail.com>
2025-12-26 21:48:54 +0400

    add request interceptor to send jwt token

--- src/shared/api/httpClient.ts
+++ src/shared/api/httpClient.ts
@@ -1,8 +1,20 @@
 import axios from "axios";
 
-export const http = axios.create({ baseURL: "https://localhost:7144/api" });
+export const http = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
 
 http.interceptors.response.use(
   (res) => res.data,
   (err) => Promise.reject(err)
 );
+
+http.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
